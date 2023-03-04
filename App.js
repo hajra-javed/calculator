14e73ea6c93dbc2868e0c7532b9f292321d4be66
hajra-javed <hajra_95@hotmail.com>
2023-03-04 17:22:33 +0500

    made basic mathematical operator functions

---
+++ App.js
@@ -0,0 +1,37 @@
+// Add
+function add(a, b) {
+    return (a + b);
+}
+
+// Subtract
+function subtract(a, b) {
+    return (a - b);
+}
+
+// Multiply
+function multiply(a, b) {
+    let p = 0;
+        for (let i = 0; i < b; i++) {
+            p = p + a;
+        }
+    return p;
+}
+
+// Divide
+function divide(a, b){
+    let p = 0;
+    c = a;
+    for (let i = 0; i < b; i++) {
+        if (c == 0){
+            break;
+        }
+        c = c - b;
+        p++;
+    }
+return p;
+}
+
+// Operate
+function operate(operator, a, b){
+    return operator(a,b);
+}
